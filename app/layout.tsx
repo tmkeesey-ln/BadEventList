c8a891dacbeab4cf4d632e812f30176c91348a35
Mike Keesey <mikekeesey@livenation.com>
2024-08-16 13:09:55 -0700

    Initial setup

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import type { PropsWithChildren } from "react";
+import "./globals.css";
+const inter = Inter({ subsets: ["latin"] });
+export const metadata: Metadata = {
+  title: "Bad Event List",
+};
+export type RootLayoutProps = Readonly<PropsWithChildren>;
+export default function RootLayout({ children }: RootLayoutProps) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
